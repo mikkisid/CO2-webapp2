9409e48ebb9325b37319c09ede3a5ea057bd2513
mikkisid <michaelgirum.com@gmail.com>
2025-09-06 00:45:27 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import numpy as np
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_errori
+from sklearn.metrics import mean_absolute_error
 
 from PIL import Image
 
